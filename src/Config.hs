2dbea73cd53fbd2db467c7689d07e9721ac88325
Adrian Schurz <adrian.schurz@check24.de>
2021-11-12 20:36:56 +0100

    Add documentation

--- src/Config.hs
+++ src/Config.hs
@@ -1,3 +1,4 @@
+-- | Configuration values like window dimensions, positions, colors, etc.
 module Config
   ( frameRate,
     barColor,
@@ -13,12 +14,15 @@ where
 
 import Graphics.Gloss.Data.Color (Color, makeColor)
 
+-- | Defines the number of frames to render per second
 frameRate :: Int
 frameRate = 2
 
+-- | Defines the foreground color
 barColor :: Color
 barColor = orange
 
+-- | Defines the background color
 backgroundColor :: Color
 backgroundColor = green
 
@@ -34,20 +38,34 @@ noTransparency = 1
 tomatoWorkIntervalInMinutes :: Float
 tomatoWorkIntervalInMinutes = 25
 
+-- | The number of seconds it will take the bar to shrink to zero size and the program to exit.
 tomatoWorkIntervalInSeconds :: Float
 tomatoWorkIntervalInSeconds = tomatoWorkIntervalInMinutes * 60
 
+-- |  The title of the application window. Certain window managers / compositors can be configured to set the window
+--    position automatically depending on the window title.
 windowTitle :: String
 windowTitle = "tomato"
 
+-- | The size of the application window.
 windowSize :: (Int, Int)
 windowSize = (1912, 5)
 
+
+{- |  The default position of the application window. The actual window position will depend on the configuration
+      of your window manager / compositor.
+-}
 windowPosition :: (Int, Int)
 windowPosition = (0, 0)
 
+{- |  Measures the vertical dimensions of the bar to be drawn in pixels. This should exceed the corresponding window
+      dimension and will be derived from those in later versions.
+-}
 halfBarHeightInPixels :: Float
 halfBarHeightInPixels = 3
 
+{- |  Measures the horizontal dimensions of the bar to be drawn in pixels. This should exceed the corresponding window
+      dimension and will be derived from those in later versions.
+-}
 halfMonitorWidthInPixels :: Float
 halfMonitorWidthInPixels = 960
